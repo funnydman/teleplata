1a982767962fe18cffcab9388c25b9eceec107de
FUNNYDMAN <crazy-dzmitry@mail.ru>
2018-07-07 18:40:33 +0300

    start to write funtion for the project configuration

--- fabfile.py
+++ fabfile.py
@@ -7,24 +7,42 @@ sudo_pass = 'optimus1234'
 config = Config(overrides={'sudo': {'password': sudo_pass}})
 conn = Connection(host='autobot', config=config)
 
+PROJECT_DIR = conn.run('pwd')
+
+VENV_DIR = f'{PROJECT_DIR}/venv'
+
+REPO_URL = 'https://github.com/FUNNYDMAN/teleplata.git'
+
+database = DATABASE['database']
+
 
 def pull_repository():
     if conn.run('test -d teleplata', warn=True).failed:
-        conn.run("git clone https://github.com/FUNNYDMAN/teleplata.git")
+        conn.run(f"git clone {REPO_URL}")
 
 
-def install_postgres():
+def install_packages():
     conn.sudo("apt-get update")
-    conn.sudo("apt-get install -y postgresql postgresql-contrib")
+    conn.sudo(
+        "apt-get install -y postgresql postgresql-contrib python3-pip python-dev virtualenv")
 
 
-def configure_database():
-    password = DATABASE['password']
+def create_database():
+    res = conn.sudo(f'sudo -i -u postgres createdb {database}')
+
+
+def configure_project():
+    with conn.cd('teleplata'):
+        conn.run('source venv/bin/activate && pip install -r requirements.txt')
+    conn.put('instance/config.py',
+             '/home/optimus/teleplata/instance/config.py')
 
 
 def main():
-    # install_postgres()
-    pull_repository()
+    # install_packages()
+    # pull_repository()
+    # create_database()
+    configure_project()
 
 
 main()
