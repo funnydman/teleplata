fe1813f095b045d5b941901866c4e66850f70060
FUNNYDMAN <crazy-dzmitry@mail.ru>
2018-07-21 23:33:54 +0300

    move SearchMixin class to file mixins

--- main/common.py
+++ main/common.py
@@ -1,5 +1,6 @@
-MODEL_FIELDS = ['model', 'power', 't_con', 'x_main',
-                'y_main', 'logic', 'invertor', 'y_scan']
+MODEL_FIELDS = ['model', 'power', 't_con', 'main', 'x_main',
+                'y_main', 'logic', 'invertor',
+                'led_driver', 'y_scan', 'y_sus']
 
 BRAND_LIST = ['samsung', 'lg', 'horizont',
               'vityaz', 'philips', 'toshiba',
