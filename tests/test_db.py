a086c82de58a53efb085eddce98940ad8d79ebc2
FUNNYDMAN <crazy-dzmitry@mail.ru>
2018-07-30 16:20:17 +0300

    add tests for checking common

--- tests/test_db.py
+++ tests/test_db.py
@@ -2,11 +2,18 @@ from teleplata.admin.models import User
 from teleplata.main.models import Samsung
 
 
-# def test_load_data_to_db(app, db):
-#     with open(os.path.join(app.path_to_tests, 'test.sql')) as f:
-#         db.engine.execute(f.read())
-#     test_model = Samsung.query.filter_by(model='test').first()
-#     assert test_model is not None
+def test_brand_table_exists_in_db():
+    from teleplata.main.common import BRAND_LIST
+    from teleplata.main.utils import get_class_by_tablename
+    for item in BRAND_LIST:
+        assert get_class_by_tablename(item)
+
+
+def test_model_fields_exist():
+    from teleplata.main.common import MODEL_FIELDS
+    new_obj = Samsung(model="test1", power="some")
+    for item in MODEL_FIELDS:
+        assert item in new_obj.__table__.columns.keys()
 
 
 def test_restore_backup():
